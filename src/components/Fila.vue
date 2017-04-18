dda08dbc05fc8d7af51fc57753149b724d73ed14
oteroweb <oterolopez1990@gmail.com>
2017-04-18 15:06:58 -0400

    first

---
+++ src/components/Fila.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        filas2222222223332
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Fila',
+  data () {
+    return {
+      msg: 'Fila'
+    }
+  }
+}
+</script>
+
+
+<style>
+</style>
---
+++ src/components/Principal.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+    estoy en {{msg}}
+        <ul v-sortable>
+        <li> aa<fila ></fila>         </li>
+        <li> bb<fila ></fila>         </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+window.vSortable
+import Fila from './Fila.vue'
+export default {
+  name: 'Principal',
+  data () {
+    return {
+      msg: 'Titulo Principal'
+    }
+  },
+  components: {
+    Fila
+  }
+}
+</script>
+
+
+<style>
+</style>
